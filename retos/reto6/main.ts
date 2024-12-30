8089035ceb00e5f282375c2a5d74b49e83bb6447
Luis Padron <luispadron25@gmail.com>
2024-12-30 13:55:03 -0500

    feat: Reto 6 logic added

--- retos/reto6/main.ts
+++ retos/reto6/main.ts
@@ -1,3 +1,11 @@
 function inBox(box: string[]): boolean {
+  // Itirate and ignore first and last row
+  for (let i = 1; i < box.length - 1; i++) {
+    const row = box[i];
+    // Find it there is "*" in the middle of the row
+    if (row.indexOf("*") > 0 && row.indexOf("*") < row.length - 1) {
+      return true;
+    }
+  }
   return false;
 }
