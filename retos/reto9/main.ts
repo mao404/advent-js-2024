bb7e0837c0b60156a4fc629762f96f220c23cea7
Luis Padron <luispadron25@gmail.com>
2025-01-03 20:09:50 -0500

    Reto 9

---
+++ retos/reto9/main.ts
@@ -0,0 +1,8 @@
+type Board = string[];
+type Movement = "U" | "D" | "R" | "L";
+type Result = "none" | "crash" | "eat";
+
+function moveTrain(board: Board, mov: Movement): Result {
+  // Code here
+  return "none";
+}
